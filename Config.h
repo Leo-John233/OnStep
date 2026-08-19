0a3eedbf33ab2d06989566d77051afb206fe5baa
Leo-John233 <badshroud233@gmail.com>
2026-08-19 09:34:03 +0800

    测试worksapce

--- Config.h
+++ Config.h
@@ -1,6 +1,6 @@
 // ---------------------------------------------------------------------------------------------------------------------------------
 // Configuration for OnStep
-
+//测试
 /*
  *          For more information on setting OnStep up see http://www.stellarjourney.com/index.php?r=site/equipment_onstep 
  *                      and join the OnStep Groups.io at https://groups.io/g/onstep
