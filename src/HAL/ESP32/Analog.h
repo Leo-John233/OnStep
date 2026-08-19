e554a76d3eed0c5671cdc5dd7f1fa54e444f8a83
Linyu <1747360050@qq.com>
2026-08-20 01:06:44 +0800

    修复ESP32核心tone()默认参数冲突导致的编译错误

--- src/HAL/ESP32/Analog.h
+++ src/HAL/ESP32/Analog.h
@@ -51,7 +51,7 @@ void __toneStop(void * pvParameters)
   vTaskDelete(NULL);
 }
 
-__attribute__ ((weak)) void tone(uint8_t pin, unsigned int frequency, unsigned long duration = 0)
+__attribute__ ((weak)) void tone(uint8_t pin, unsigned int frequency, unsigned long duration)
 {
   portENTER_CRITICAL(&__analogOutMux);
   int channel=__pwmAllocateChannel(pin);
